692a7946bc87f8a503ab466090285ad9210f0192
Simon <kharitonov.simon@yandex.ru>
2019-09-13 12:03:18 +0300

    code for debug

--- my_lessons/my_lessons_3.cpp
+++ my_lessons/my_lessons_3.cpp
@@ -2,9 +2,48 @@
 #include <stdio.h>
 #include <Windows.h>
 #include <iostream>
+#include <string>
 
 using namespace std;
 
+string mySecondFunction(string p) {
+	return "Result :" + p + " ! \n";
+}
+
+string myFunctionA(char x[], char y[]) {
+	string d, s, z;
+
+	d = string(x);
+	s = string(y);
+
+	z = d + s;
+
+	return mySecondFunction(z);
+}
+
+string myThirdFunction(char x[], string y) {
+	if (string(x) == y) {
+		return "Good!";
+	} else {
+		return "Not good...";
+	}
+}
+
+int main() {
+
+	char a[10] = "123";
+	char b[5] = "456";
+	char c[10];
+
+	cout << "Pease enter: " + myFunctionA(a, b);
+	cin >> c;
+
+	cout << myThirdFunction(c, myFunctionA(a, b));
+
+	return 0;
+}
+
+/*
 int main() {
 
 	const int arrayLength = 20;
@@ -26,6 +65,7 @@ int main() {
 
 	return 0;
 }
+*/
 
 
 /*
