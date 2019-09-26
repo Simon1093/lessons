638ffffa723df1a00fc03c7bf0b16ce50c085235
Simon <kharitonov.simon@yandex.ru>
2019-09-26 11:34:08 +0300

    fopen

--- my_lessons/my_lessons_5.cpp
+++ my_lessons/my_lessons_5.cpp
@@ -7,31 +7,78 @@
 
 using namespace std;
 
-int main() {
-	/*ofstream file;
-	file.open("test.txt");
-	file << "qweqweqwe asdasdas sdfsdfsdf asdasdqwq232";
-	file.close();
-	*/
+void addUser(char userData[]) {
+	FILE *file;
 
-	char text[200], text1[200];
+	fopen_s(&file, "file.txt", "a");
 
-	ifstream file("test.txt");
-	if (!file.is_open()) {
-		cout << "Error! File not found!";
-	} else {
-
-	/*	file >> text;
-		cout << text << endl;*/
-
-		while (file.getline(text1, 200)) {
-			cout << text1 << endl;
-		}
-		file.close();
+	if (file != NULL)
+	{
+		fputs(userData, file); // write string in file
+		fclose(file);
 	}
+}
+
+void getUserData() {
+	FILE *file;
+
+	fopen_s(&file, "file.txt", "r");
+
+	char userName[20], userLastName[20], userSurname[20];
+
+	if (file != NULL)
+	{
+		while (!feof(file)) {
+			fscanf_s(file, "%s %s %s\n", userName, 20, userLastName, 20, userSurname, 20); // read string from file
+			printf("%s %s %s\n", userName, userLastName, userSurname);
+		}
+		
+		fclose(file);
+	}
+}
+
+
+
+int main() {
+	char userData[] = "Ivanov Ivan Petrovich\n";
+
+	addUser(userData);
+
+	system("pause");
+
+	getUserData();
+
+	system("pause");
 
 	return 0;
 }
+
+//int main() {
+//	/*ofstream file;
+//	file.open("test.txt");
+//	file << "qweqweqwe asdasdas sdfsdfsdf asdasdqwq232";
+//	file.close();
+//	*/
+//
+//	char text[200], text1[200];
+//
+//	ifstream file("test.txt");
+//	if (!file.is_open()) {
+//		cout << "Error! File not found!";
+//	} else {
+//
+//	/*	file >> text;
+//		cout << text << endl;*/
+//
+//		while (file.getline(text1, 200)) {
+//			cout << text1 << endl;
+//		}
+//		file.close();
+//	}
+//
+//	return 0;
+//}
+
 
 /*
 int main() {
